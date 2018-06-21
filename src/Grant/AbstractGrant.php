a9e1aecff65d8b1baa2d36b2d481302b1935a3c5
Hassan Amouhzi <github@anezi.net>
2018-06-21 09:08:47 +0200

    Update AbstractGrant.php

--- src/Grant/AbstractGrant.php
+++ src/Grant/AbstractGrant.php
@@ -30,8 +30,6 @@ use League\OAuth2\Client\Tool\RequiredParameterTrait;
  */
 abstract class AbstractGrant
 {
-    use RequiredParameterTrait;
-
     /**
      * Returns the name of this grant, eg. 'grant_name', which is used as the
      * grant type when encoding URL query parameters.
@@ -76,5 +74,38 @@ abstract class AbstractGrant
         $this->checkRequiredParameters($required, $provided);
 
         return $provided;
+    }
+    
+    /**
+     * Checks for a required parameter in a hash.
+     *
+     * @throws BadMethodCallException
+     * @param  string $name
+     * @param  array  $params
+     * @return void
+     */
+    private function checkRequiredParameter($name, array $params)
+    {
+        if (!isset($params[$name])) {
+            throw new BadMethodCallException(sprintf(
+                'Required parameter not passed: "%s"',
+                $name
+            ));
+        }
+    }
+    
+   /**
+     * Checks for multiple required parameters in a hash.
+     *
+     * @throws InvalidArgumentException
+     * @param  array $names
+     * @param  array $params
+     * @return void
+     */
+    private function checkRequiredParameters(array $names, array $params)
+    {
+        foreach ($names as $name) {
+            $this->checkRequiredParameter($name, $params);
+        }
     }
 }
